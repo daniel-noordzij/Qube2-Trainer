991bbd09c5778767f5e2dd61003cd30725212d0f
MonsterDruide1 <5958456@gmail.com>
2020-03-13 16:48:35 +0100

    Divided into UI and Trainer

--- QubeTrainer/Form2.cs
+++ QubeTrainer/Form2.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace QubeTrainer
+namespace QubeTrainerNamespace
 {
     public partial class Form2 : Form
     {
